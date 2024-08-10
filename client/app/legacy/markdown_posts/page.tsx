4baa17a0ceec07d2dad9eb535f9d96018cb09c82
horiyee <kaito@horiyee.dev>
2024-08-10 21:32:34 +0900

    feat: fetch legacy markdown posts

--- client/app/legacy/markdown_posts/page.tsx
+++ client/app/legacy/markdown_posts/page.tsx
@@ -1,16 +1,66 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchLegacyMarkdownPostsApiClient } from "../../api/client/legacy";
+import { LegacyMarkdownPost } from "../../types/legacy";
 
 const LegacyMarkdownPostsPage: React.FC = () => {
+  const [markdownPosts, setMarkdownPosts] = useState<LegacyMarkdownPost[]>(
+    process.env.NODE_ENV === "development"
+      ? [
+          {
+            id: 1,
+            title: "タイトル",
+            body: "# 本文",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            categoryId: 1,
+            categoryName: "カテゴリ1",
+            publish: true,
+          },
+          {
+            id: 2,
+            title: "タイトル",
+            body: "# 本文",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            categoryId: 2,
+            categoryName: "カテゴリ2",
+            publish: false,
+          },
+        ]
+      : [],
+  );
+
   useEffect(() => {
-    fetchLegacyMarkdownPostsApiClient().then(console.log);
+    fetchLegacyMarkdownPostsApiClient()
+      .then(setMarkdownPosts)
+      .catch(console.error);
   }, []);
 
   return (
     <main className="container">
       <h1>マークダウン記事</h1>
+
+      <section>
+        {markdownPosts.map((markdownPost) => (
+          <article>
+            <form key={markdownPost.id}>
+              <fieldset>
+                <label>
+                  タイトル
+                  <input type="text" value={markdownPost.title} />
+                </label>
+
+                <label>
+                  本文
+                  <textarea value={markdownPost.body} />
+                </label>
+              </fieldset>
+            </form>
+          </article>
+        ))}
+      </section>
     </main>
   );
 };
