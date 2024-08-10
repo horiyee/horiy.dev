1f5bd4900d9424691c0c223aec49325d381c9230
horiyee <kaito@horiyee.dev>
2024-08-11 02:10:41 +0900

    feat: implement notes

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,8 +1,13 @@
+import createMDX from "@next/mdx";
+import remarkGFM from "remark-gfm";
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: "export",
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+  reactStrictMode: true,
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
@@ -14,4 +19,12 @@ const nextConfig = {
   // distDir: 'dist',
 };
 
-module.exports = nextConfig;
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGFM],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
