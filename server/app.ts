729d1c7f9ec9fee5d0109eef05ddf8f853e9afdb
horiyee <kaito@horiyee.dev>
2024-08-12 14:48:06 +0900

    chore: delete health check api

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,7 @@
 import { Application } from "./deps.ts";
-import healthCheck from "./routes/healthCheck.ts";
 import bookmark from "./routes/bookmark.ts";
 
 const app = new Application();
-
-app.use(healthCheck.routes());
 
 app.use(bookmark.routes());
 
